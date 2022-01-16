82ca658deda3d296c6999440ea158b1fd254c0a2
Ardavan <ardavaneskandari007@gmail.com>
2022-01-16 16:24:13 +0330

    Files
    Web Files

---
+++ main.js
@@ -0,0 +1,13 @@
+function openBar() {
+
+    document.getElementById("Sideb").style.width = "200px";
+
+    document.getElementById("main").style.marginLeft = "200px";
+}
+  
+function closeBar() {
+
+    document.getElementById("Sideb").style.width = "0";
+
+    document.getElementById("main").style.marginLeft = "0";
+}
